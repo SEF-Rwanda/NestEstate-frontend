2d9a9c42a443dcc836115395ee61c44e7c7c48bc
ngirimana <chadrackngirimana@gmail.com>
2023-02-26 23:30:07 +0200

    change length of input

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import FooterComponent from "../component/FooterComponent";
+import HeaderComponent from "../component/HeaderComponent";
+
+import PropertyCarouselComponent from "../component/PropertyCarouselComponent";
+import HouseCardComponent from "../component/HouseCardComponent";
 
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <PropertyCarouselComponent />
+      <HouseCardComponent />
       <FooterComponent />
     </div>
   );
