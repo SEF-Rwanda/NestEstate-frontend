2a5b4acd82f57e42b4d5e790c5fbfc3ca3555926
shema19974 <musonerwaprince@gmail.com>
2023-03-06 09:24:44 +0200

    Design the login according to figma

--- src/pages/user/Login.jsx
+++ src/pages/user/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-
+import Input from "../../component/Input"
 import { Form, Button, Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
@@ -35,52 +35,51 @@ const Login = () => {
     }
   };
   return (
-    <div className="container">
-      <div className="row justify-content-left">
-        <div className="col-md-4">
-          <div style={{ marginTop: "25px" }}>
-            <h5 className="login-text">Sign In to Real Estate Application</h5>
+    <>
+      <Container style={{ height: "calc(100vh - 8vh)" }} className="d-flex justify-content-center align-items-center" >
+        <Row className="d-flex justify-content-center align-items-center">
+          <h1 className="text-center" style={{ fontSize: "25px", fontWeight: "bold", margin: "25px" }}>Login Here</h1>
+          <Col xs="4">
             {error && <p style={{ color: "red" }}>{error}</p>}
             <Form onSubmit={handleSubmit} className="login-form">
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control
-                  className="input-form"
-                  onChange={(e) => setEmail(e.target.value)}
-                  type="email"
-                  placeholder="Enter email"
-                />
-              </Form.Group>
-
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  onChange={(e) => setPassword(e.target.value)}
-                  type="password"
-                  placeholder="Password"
-                />
-              </Form.Group>
-              <Link to="/send-email"> Forgot Password ? </Link>
-              <br />
-              <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Remember Me" />
-              </Form.Group>
-              <Button variant="primary" type="submit">
-                Submit
-              </Button>
+              <Input
+                controlId="email"
+                labelText="First Name"
+                type="email"
+                name="email"
+                placeholder="Enter your Email"
+                value={email}
+                onChange={setEmail}
+              />
+              <Input
+                controlId="password"
+                labelText="Password"
+                type="password"
+                name="passwrod"
+                placeholder="Enter password"
+                value={password}
+                onChange={setPassword}
+              />
+              <div style={{ marginLeft: "140px" }}>
+                <Link to="/send-email" style={{ color: "#6736CF" }}> Forgot Password ? </Link>
+                <br />
+                <Button style={{ backgroundColor: "#6736CF", border: "none", borderRadius: "25px", width: "100px", fontWeight: "bold", marginTop: "15px", marginBottom: "15px" }} type="submit">
+                  Login
+                </Button>
+                <p style={{ fontSize: "15px" }}>Don't have an account? Click <Link to="/register" style={{ color: "#6736CF" }}> here</Link></p>
+              </div>
             </Form>
-          </div>
-        </div>
-        <div className="col-md-4">
-          {/* Add a photo */}
-          <img
-            src="images/login_undraw.svg"
-            alt="login"
-            style={{ width: "100%", height: "100%" }}
-          />
-        </div>
-      </div>
-    </div>
+          </Col>
+          <Col xs="4">
+            <img
+              src="images/login_undraw.svg"
+              alt="login"
+              style={{ width: "100%", height: "100%" }}
+            />
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
