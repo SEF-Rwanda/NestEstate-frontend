48a8dc63be69d70d018914ff500087dc45e1140d
shema19974 <musonerwaprince@gmail.com>
2023-03-06 10:15:03 +0200

    Customize the homepage to receive dynamic properties

--- src/component/HouseCardComponent.js
+++ src/component/HouseCardComponent.js
@@ -1,81 +1,61 @@
 import { Col, Row, Card, Container } from "react-bootstrap"
 const CategoryCardComponent = ({ category, id }) => {
-    const cardData = [
+    const propertiesData = [
         {
             id: 1,
             imageUrl: "images/house-1.jpg",
-            title: "Card 1",
-            description: "This is the description for card 1",
+            description: "Furnished House For Rent in Kacyiru",
+            bedrooms: 3,
+            bathrooms: 2,
+            price: 100000,
+            type: "Rent"
         },
         {
             id: 2,
             imageUrl: "images/house-1.jpg",
-            title: "Card 2",
-            description: "This is the description for card 2",
+            description: "Furnished House For Sale in Remera",
+            bedrooms: 4,
+            bathrooms: 2,
+            price: 200000,
         },
         {
             id: 3,
             imageUrl: "images/house-1.jpg",
-            title: "Card 3",
-            description: "This is the description for card 3",
+            description: "Furnished House For Rent in Kanombe",
+            bedrooms: 6,
+            bathrooms: 3,
+            price: 150000,
+        },
+        {
+            id: 3,
+            imageUrl: "images/house-1.jpg",
+            description: "Furnished House For Rent in Kacyiru",
+            bedrooms: 10,
+            bathrooms: 4,
+            price: 300000,
         },
     ];
 
     return (
         <Container>
-            {/* Make a loop based on card data */}
+            <h4 style={{ marginTop: "10px", marginBottom: "10px", textAlign: "center" }}>Find Recent Properties</h4>
+            <Row>
+                {propertiesData.map((property) => {
+                    return (
 
-            <Row className="p-2">
-                <Col md={4} sm={6}>
-                    <Card>
-                        <Card.Img variant="top" src="images/house-1.jpg" style={{ height: "200px", objectFit: "cover", objectPosition: "center" }} />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up
-                                the bulk of the card's content.
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col md={4} sm={6}>
-                    <Card>
-                        <Card.Img variant="top" src="images/house-1.jpg" style={{ height: "200px", objectFit: "cover" }} />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up
-                                the bulk of the card's content.
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col md={4} sm={6}>
-                    <Card>
-                        <Card.Img variant="top" src="images/house-1.jpg" style={{ height: "200px", objectFit: "cover" }} />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up
-                                the bulk of the card's content.
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-            <Row className="p-2">
-                <Col md={4} sm={6}>
-                    <Card>
-                        <Card.Img variant="top" src="images/house-1.jpg" style={{ height: "200px", objectFit: "cover" }} />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up
-                                the bulk of the card's content.
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
+                        <Col md={4} sm={6} className="mt-3">
+                            <Card>
+                                <Card.Img variant="top" src={property.imageUrl} style={{ height: "200px", objectFit: "cover" }} />
+                                <Card.Body>
+                                    {/* <Card.Title>{property.title}</Card.Title> */}
+                                    <Card.Text>{property.description}</Card.Text>
+                                    <p> {property.bedrooms} Bedrooms & {property.bathrooms} bathrooms</p>
+                                    <p style={{fontWeight: "bold", fontSize: "18px"}}>$ {property.price}</p>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    );
+                })}
             </Row>
         </Container>
     );
