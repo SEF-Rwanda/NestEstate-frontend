0fd9edc3803c82d5afff33dcc99b142184837647
ngirimana <chadrackngirimana@gmail.com>
2023-04-13 19:21:50 +0200

    server ip

--- src/state/message/MessageSlice.js
+++ src/state/message/MessageSlice.js
@@ -1,6 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-import jwt_decode from "jwt-decode";
 
 const baseAPIUrl = "/api/v1";
 const token = localStorage.getItem("token");
